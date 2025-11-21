98bd8eb1e58d6c3412cd18bc78706a65186bcc61
Priyanshu Sharma <priyanshusharma1131@gmail.com>
2025-11-21 16:35:13 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const themeBtn = document.getElementById('themeBtn');
-themeBtn.addEventListener('click', () => {
-    document.body.classList.toggle('light-theme');
-    if(document.body.classList.contains('light-theme')) {
-        themeBtn.textContent = '🌞';
-    } else {
-        themeBtn.textContent = '🌙';
-    }
-});
